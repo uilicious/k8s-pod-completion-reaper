75a363c5a1200edf1f64d7454ca5ffddaddd0e29
remy <remy@uilicious.com>
2021-10-26 16:47:48 +0800

    fix

--- hooks/pods-hook.sh
+++ hooks/pods-hook.sh
@@ -13,19 +13,19 @@ else
   podName=$(jq -r '.[0].object.metadata.name' ${BINDING_CONTEXT_PATH})
   podReason=$(jq -r '.[0].object.status.containerStatuses[0].state.terminated.reason' ${BINDING_CONTEXT_PATH}) #${BINDING_CONTEXT_PATH} is a path to .json file
   if [[ $podReason == *'Unhealthy'* ]]; then
-    echo 'DEBUG - pod ${podName} is unhealthy'
+    echo "DEBUG - pod ${podName} is unhealthy"
   fi
   if [[ $podReason == *'Completed'* ]]; then #Check if  logs contain "reason": "Completed" (might want to go for "CrashLoopBackOff" if this is too frequent)
     if [ $DEBUG = 'true' ] ; then
-      echo 'DEBUG - would delete pod ${podName}, reason: ${podReason}'
+      echo "DEBUG - would delete pod ${podName}, reason: ${podReason}"
     else
       if  [[  -z  $TARGET  ]] ; then
         kubectl delete pod --wait=false $podName 
-        echo 'ACTION - target is entier namespace, deleting pod ${podName}, reason: ${podReason}'
+        echo "ACTION - target is entier namespace, deleting pod ${podName}, reason: ${podReason}"
       else
         if [[ $podName =~ $TARGET ]] ; then
           kubectl delete pod --wait=false $podName 
-          echo 'ACTION - target is ${TARGET}, deleting pod ${podName}, reason: ${podReason}'
+          echo "ACTION - target is ${TARGET}, deleting pod ${podName}, reason: ${podReason}"
         fi
       fi
     fi

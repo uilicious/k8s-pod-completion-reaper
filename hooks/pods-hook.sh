06ca36a79c1e960a451a340152a740def45a5bd4
remy <remy@uilicious.com>
2021-10-26 15:42:29 +0800

    fix macos cluster issue

--- hooks/pods-hook.sh
+++ hooks/pods-hook.sh
@@ -11,13 +11,16 @@ kubernetes:
 EOF
 else
   podReason=$(jq -r '.[0].object.status.containerStatuses[0].state.terminated.reason' ${BINDING_CONTEXT_PATH}) #${BINDING_CONTEXT_PATH} is a path to .json file
+  if [[ $podReason == *'Unhealthy'* ]]; then
+    echo 'DEBUG - POD IS UNHEALTHY'
+  fi
   if [[ $podReason == *'Completed'* ]]; then #Check if  logs contain "reason": "Completed" (might want to go for "CrashLoopBackOff" if this is too frequent)
     podName=$(jq -r '.[0].object.metadata.name' ${BINDING_CONTEXT_PATH})
     if [ $DEBUG = 'true' ] ; then
       echo 'DEBUG - POD STATUS IS COMPLETED - WOULD DELETE:' $podName
     else
       if  [[  -z  $TARGET  ]] ; then
-        kubectl delete pod $podName 
+        kubectl delete pod --wait=false $podName 
         echo 'POD STATUS IS COMPLETED - DELETED:' $podName
       else
         if [[ $podName =~ $TARGET ]] ; then

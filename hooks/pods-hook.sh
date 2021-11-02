b04285a7594de701724cd8f8bed8b96b30904dc6
Eugene Cheah <eugeneqin@gmail.com>
2021-11-02 06:14:54 +0000

    Revised pods-hook.sh

--- hooks/pods-hook.sh
+++ hooks/pods-hook.sh
@@ -1,38 +1,121 @@
 #!/usr/bin/env bash
 
+#-----------------------------------------------------------------------------------------
+#
+# This is called once by shell-operator, using the "--config" parameter
+#
+# See: https://github.com/flant/shell-operator#build-an-image-with-your-hooks
+#
+# It is used to configure the pod hook, and limit to events according to
+# the defined filters
+#
+#-----------------------------------------------------------------------------------------
 if [[ $1 == "--config" ]] ; then
-  cat <<EOF
+cat <<EOF
 configVersion: v1
 kubernetes:
 - apiVersion: v1
   kind: Pod
+  # 
+  # Because the modified event is extreamly verbose,
+  # (especially with healthcheck, which updates it every X seconds)
+  # when possible we will add upfront any filters we can
+  # use to reduce the amount of "modified" events
+  #
   executeHookOnEvent:
   - Modified
+  # 
+  # Limit filtering to the namespace
+  #
   namespace:
     nameSelector:
-      matchNames: ["${TARGETNAMESPACE}"]
+      matchNames: ["${NAMESPACE}"]
+  #
+  # Limit filtering to changes in "state"
+  # note that i suspect this is done on the shell-operator
+  # side (not the k8s cluster) so its effectiveness is
+  # questionable - but better then nothing
+  #
+  jqFilter: ".status.containerStatuses[0].state.terminated"
 EOF
+# Exit immediately, after outputting the config
+exit 0
+fi
+#-----------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------
+#
+# Handle the processing of events.
+#
+# ${BINDING_CONTEXT_PATH} is a path to .json file
+# 
+#-----------------------------------------------------------------------------------------
+
+# Get the raw JSON event string, we intentionally do this only once
+# to reduce the amount of IO involved in temporary files
+JSON_EVENT_STR=$(cat ${BINDING_CONTEXT_PATH})
+
+# Get the JSON object
+JSON_OBJ_STR=$(echo $JSON_EVENT_STR | jq '.[0].object')
+
+# Lets extract out several key values
+POD_NAME=$(echo "$JSON_OBJ_STR" | jq '.metadata.name')
+
+# Lets skip the pods whose names do not match
+if [[ -z "$TARGETPOD" ]]; then
+  # TARGETPOD parameter is empty, match all containers in namespace
 else
-  podName=$(jq -r '.[0].object.metadata.name' ${BINDING_CONTEXT_PATH})
-  podReason=$(jq -r '.[0].object.status.containerStatuses[0].state.terminated.reason' ${BINDING_CONTEXT_PATH}) #${BINDING_CONTEXT_PATH} is a path to .json file
-  if [[ $podReason == *'Unhealthy'* ]]; then
-    echo "DEBUG - pod ${podName} in namespace ${TARGETNAMESPACE} is unhealthy"
-  fi
-  if [[ $podReason == *'Completed'* ]]; then #Check if  logs contain "reason": "Completed" (might want to go for "CrashLoopBackOff" if this is too frequent)
-    if [ $DEBUG = 'true' ] ; then
-      echo "DEBUG - would delete pod ${podName} in namespace ${TARGETNAMESPACE}, reason: ${podReason}"
-    else
-      if  [[  -z  $TARGETPOD  ]] ; then
-        kubectl delete pod --wait=false $podName 
-        echo "ACTION - target is any pods in namespace ${TARGETNAMESPACE}, deleting pod ${podName}, reason: ${podReason}"
-      else
-        if [[ $podName =~ $TARGETPOD ]] ; then
-          kubectl delete pod --wait=false $podName 
-          echo "ACTION - target is ${TARGETPOD} in namespace ${TARGETNAMESPACE}, deleting pod ${podName}, reason: ${podReason}"
-        fi
-      fi
+  if [[ "$POD_NAME"=~"$TARGETPOD" ]]; then
+    # TARGETPOD matches, we shall permit this event
+  else
+    # TARGETPOD does not match, we should skip this event
+    if [[ "$DEBUG"=="true "]]; then
+      echo "DEBUG - skipping ${POD_NAME} as it does not match TARGETPOD regex : $TARGETPOD"
     fi
+    exit 0
   fi
 fi
 
+# Get the terminated exit code and reason
+TERMINATED_EXITCODE=$(echo "$JSON_OBJ_STR" | jq '.status.containerStatuses[0].state.terminated.exitCode')
+TERMINATED_REASON=$(echo "$JSON_OBJ_STR" | jq '.status.containerStatuses[0].state.terminated.reason')
 
+# Fallback to "lastState", this can happen if the contianer is started "quickly"
+# before the event is properlly handled - and/or - the original event was missed
+if [[ -z "$TERMINATED_EXITCODE" ]] || [[ "$TERMINATED_EXITCODE"=="null" ]]; then
+  TERMINATED_EXITCODE=$(echo "$JSON_OBJ_STR" | jq '.status.containerStatuses[0].lastState.terminated.exitCode')
+  TERMINATED_REASON=$(echo "$JSON_OBJ_STR" | jq '.status.containerStatuses[0].lastState.terminated.reason')
+fi
+
+# If there is no exitcode / reason, we presume its a misfied event
+# so we shall ignore it
+if [[ -z "$TERMINATED_EXITCODE" ]] || [[ "$TERMINATED_EXITCODE"=="null" ]]; then
+  exit 0
+fi
+
+# Special handling of exit code 0
+if [[ "$APPLY_ON_EXITCODE_0"!="true" ]]; then
+  # Check if exit code is 0, skip it
+  if [[ "$TERMINATED_EXITCODE"=="0" ]]; then
+    # DEBUG log the container that was skipped
+    if [[ "$DEBUG"=="true "]]; then
+      echo "DEBUG - skipping ${podName} with exitcode 0 ( APPLY_ON_EXITCODE_0 != true )"
+    fi
+    exit 0
+  fi
+fi
+
+#
+# We are here, lets do the termination event !
+#
+
+# Lets handle debug mode
+if [[ "$DEBUG"=="true "]]; then
+	echo "DEBUG - would have terminated ${POD_NAME} which completed with exitcode $TERMINATED_EXITCODE : $TERMINATED_REASON"
+	exit 0
+fi
+
+# Lets perform the termination event
+echo "ACTION - terminating ${POD_NAME} which completed with exitcode $TERMINATED_EXITCODE : $TERMINATED_REASON"
+kubectl delete pod --wait=false $POD_NAME 
+
+#-----------------------------------------------------------------------------------------

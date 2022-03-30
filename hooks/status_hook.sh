2690e86911a95c62ea9c05f2f4850f5c437a7dc5
Remy <remy@uilicious.com>
2022-03-30 10:56:02 +0000

    Update status_hook.sh

--- hooks/status_hook.sh
+++ hooks/status_hook.sh
@@ -5,66 +5,53 @@ configVersion: v1
 kubernetes:
 - apiVersion: v1
   kind: Pod
-  # 
-  # Because the modified event is extreamly verbose,
-  # (especially with healthcheck, which updates it every X seconds)
-  # when possible we will add upfront any filters we can
-  # use to reduce the amount of "modified" events
   #
   executeHookOnEvent:
   - Modified
   #
-  # Limit filtering to changes in "state"
+  # Limit filtering to changes in ready states (stored in conditions[1])
   #
-  jqFilter: ".status.containerStatuses[0].state.terminated"
+  jqFilter: ".status.conditions[1].status"
 EOF
-# Exit immediately, after outputting the config
 exit 0
 fi
-#-----------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------
-#
-# Handle the processing of events.
-#
-# ${BINDING_CONTEXT_PATH} is a path to .json file
-# 
-#-----------------------------------------------------------------------------------------
-
 # Get the raw JSON event string, we intentionally do this only once
 # to reduce the amount of IO involved in temporary files
 JSON_EVENT_STR=$(cat ${BINDING_CONTEXT_PATH})
 
-# Get the JSON object
-JSON_OBJ_STR=$(echo $JSON_EVENT_STR | jq -r '.[0].object')
+    #Can't check if enabled earlier in code because hooks need at least have one property
+    if [ "$ENABLE_STATUS_HOOK" = "true" ] ; then
+    # Get the JSON object
+    JSON_OBJ_STR=$(echo $JSON_EVENT_STR | jq -r '.[0].object')
+    # Lets extract out several key values
+    POD_NAME=$(echo "$JSON_OBJ_STR" | jq -r '.metadata.name')
 
-# Lets extract out several key values
-POD_NAME=$(echo "$JSON_OBJ_STR" | jq -r '.metadata.name')
+    if [[ "$POD_NAME" =~ "$TARGETPOD" ]]; then
+    # TARGETPOD matches, we shall permit this event
+	    :
+    else
+        # TARGETPOD does not match, we should skip this event
+        exit 0
+    fi
+    # SEE POD CONDITIONS FROM OFFICIAL DOC
+    # https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/
+    INIT=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[0].status')
+    # 
+    READY=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].status')
+    READY_MESSAGE=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].message')
+    READY_REASON=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].reason')
+    # 
+    CONTAINER_READY=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].status')
+    #CONTAINER_READY_MESSAGE=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].message')
+    #CONTAINER_READY_REASON=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].reason')
+    # 
+    PODSCHEDULED=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[3].status')
 
-if [[ "$POD_NAME" =~ "failed-pod" ]]; then
-  # TARGETPOD matches, we shall permit this event
-	:
-else
-  # TARGETPOD does not match, we should skip this event
-  exit 0
+    #READY status only include message if false, let's avoid confusing someone by printing null
+    if [[ "$READY" = "False" ]]; then
+        echo "STATUS UPDATE ${POD_NAME} - INIT IS ${INIT} | READY IS ${READY} - ${READY_MESSAGE} : ${READY_REASON} | CONTAINER READY IS ${CONTAINER_READY} | POD SCHEDULED IS ${PODSCHEDULED} "
+    else 
+        echo "STATUS UPDATE ${POD_NAME} - INIT IS ${INIT} | READY IS ${READY} | CONTAINER READY IS ${CONTAINER_READY} | POD SCHEDULED IS ${PODSCHEDULED} "
+    fi
+
 fi
-
-# Get the terminated exit code and reason
-TERMINATED_EXITCODE=$(echo "$JSON_OBJ_STR" | jq -r '.status.containerStatuses[0].state.terminated.exitCode')
-TERMINATED_REASON=$(echo "$JSON_OBJ_STR" | jq -r '.status.containerStatuses[0].state.terminated.reason')
-
-# Fallback to "lastState", this can happen if the contianer is started "quickly"
-# before the event is properlly handled - and/or - the original event was missed
-if [[ -z "$TERMINATED_EXITCODE" ]] || [[ "$TERMINATED_EXITCODE" == "null" ]]; then
-  TERMINATED_EXITCODE=$(echo "$JSON_OBJ_STR" | jq '.status.containerStatuses[0].lastState.terminated.exitCode')
-  TERMINATED_REASON=$(echo "$JSON_OBJ_STR" | jq '.status.containerStatuses[0].lastState.terminated.reason')
-fi
-
-# If there is no exitcode / reason, we presume its a misfied event
-# so we shall ignore it
-if [[ -z "$TERMINATED_EXITCODE" ]] || [[ "$TERMINATED_EXITCODE" == "null" ]]; then
-  exit 0
-fi
-
-# Lets perform the termination event
-echo "ACTION - terminating ${POD_NAME} which completed with exitcode ${TERMINATED_EXITCODE} : ${TERMINATED_REASON}"
-kubectl delete pod --wait=false $POD_NAME 

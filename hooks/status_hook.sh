02d2e8458e354cbe99f579ada20e1fc31efdc43e
rplauta <rplauta@gmail.com>
2022-04-15 23:52:41 +0800

    Disable status_hook triggers with flag

--- hooks/status_hook.sh
+++ hooks/status_hook.sh
@@ -17,7 +17,8 @@
 # the defined filters
 #
 #-----------------------------------------------------------------------------------------
-if [[ $1 == "--config" ]] ; then
+if [[ "$LOG_STATUS_CHANGE" = "true" ]]; then
+  if [[ $1 == "--config" ]] ; then
 cat <<EOF
 configVersion: v1
 kubernetes:
@@ -25,7 +26,7 @@ kubernetes:
   kind: Pod
   # 
   # Because the modified event is extreamly verbose,
-  # (especially with healthcheck, which updates it every X seconds)
+  #(especially with healthcheck, which updates it every X seconds)
   # when possible we will add upfront any filters we can
   # use to reduce the amount of "modified" events
   #
@@ -42,9 +43,32 @@ kubernetes:
   #
   jqFilter: ".status.conditions[1].status"
 EOF
-# Exit immediately, after outputting the config
-exit 0
+  # Exit immediately, after outputting the config
+  exit 0
+  fi
+# Even if we don't want the hook to trigger shell operator hooks needs a config, 
+# Let's just add one that will never trigger.
+else
+  if [[ $1 == "--config" ]] ; then
+cat <<EOF
+configVersion: v1
+kubernetes:
+- apiVersion: v1
+  kind: Pod
+  executeHookOnEvent:
+  - Deleted
+  # 
+  # Limit filtering to the empty namespace so it doesn't trigger
+  #
+  namespace:
+    nameSelector:
+      matchNames: ["hookwillnottrigger"]
+EOF
+  # Exit immediately, after outputting the config
+  exit 0
+  fi
 fi
+
 #-----------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------
 #
@@ -71,34 +95,31 @@ if [[ -z "$TARGETPOD" ]]; then
 else
   if [[ "$POD_NAME" =~ "$TARGETPOD" ]]; then
     # TARGETPOD matches, we shall permit this event
-		:
+		  :
   else
     # TARGETPOD does not match, we should skip this event
     exit 0
   fi
 fi
 
-# Can't check if enabled earlier in code because hooks need at least have one property
-# IF STATUS LOGS AREN'T SHOWING UP MAKE SURE LOG_LEVEL IS SET TO "INFO"
-if [[ "$LOG_STATUS_CHANGE" = "true" ]]; then
-    # SEE POD CONDITIONS FROM OFFICIAL DOC
-    # https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/
-    INIT=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[0].status')
-    # 
-    READY=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].status')
-    READY_MESSAGE=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].message')
-    READY_REASON=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].reason')
-    # 
-    CONTAINER_READY=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].status')
-    #CONTAINER_READY_MESSAGE=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].message')
-    #CONTAINER_READY_REASON=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].reason')
-    # 
-    PODSCHEDULED=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[3].status')
+# SEE POD CONDITIONS FROM OFFICIAL DOC
+# https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/
+INIT=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[0].status')
+# 
+READY=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].status')
+READY_MESSAGE=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].message')
+READY_REASON=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[1].reason')
+# 
+CONTAINER_READY=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].status')
+#CONTAINER_READY_MESSAGE=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].message')
+#CONTAINER_READY_REASON=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[2].reason')
+# 
+PODSCHEDULED=$(echo "$JSON_OBJ_STR" | jq -r '.status.conditions[3].status')
 
-    #READY status only include message if false, let's avoid confusing someone by printing null
-    if [[ "$READY" = "False" ]]; then
-        echo "STATUS UPDATE ${POD_NAME} - INIT IS ${INIT} | READY IS ${READY} - ${READY_MESSAGE} : ${READY_REASON} | CONTAINER READY IS ${CONTAINER_READY} | POD SCHEDULED IS ${PODSCHEDULED} "
-    else 
-        echo "STATUS UPDATE ${POD_NAME} - INIT IS ${INIT} | READY IS ${READY} | CONTAINER READY IS ${CONTAINER_READY} | POD SCHEDULED IS ${PODSCHEDULED} "
-    fi
+#READY status only include message if false, let's avoid confusing someone by printing null
+# IF STATUS LOGS AREN'T SHOWING UP MAKE SURE LOG_LEVEL IS SET TO "INFO"
+if [[ "$READY" = "False" ]]; then
+  echo "STATUS UPDATE ${POD_NAME} - INIT IS ${INIT} | READY IS ${READY} - ${READY_MESSAGE} : ${READY_REASON} | CONTAINER READY IS ${CONTAINER_READY} | POD SCHEDULED IS ${PODSCHEDULED} "
+else 
+  echo "STATUS UPDATE ${POD_NAME} - INIT IS ${INIT} | READY IS ${READY} | CONTAINER READY IS ${CONTAINER_READY} | POD SCHEDULED IS ${PODSCHEDULED} "
 fi
